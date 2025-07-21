ccf5c29736b7ab749984b7afa4b132978ed5e170
Cole Lopez <lopezcole.dv@gmail.com>
2025-07-21 16:13:02 +0200

    Remove unused import for PasswordField in EmailSettingsController

--- src/main/java/com/cole/controller/EmailSettingsController.java
+++ src/main/java/com/cole/controller/EmailSettingsController.java
@@ -2,7 +2,6 @@ package com.cole.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.Alert;
 import java.util.Properties;
 import java.io.*;
