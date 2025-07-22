c074833d61c557b8420e2db3512a51a9ccdc4a0e
Cole Lopez <lopezcole.dv@gmail.com>
2025-07-22 17:02:23 +0200

    Add GraduatesController class with initialize method

---
+++ src/main/java/com/cole/controller/GraduatesController.java
@@ -0,0 +1,12 @@
+package com.cole.controller;
+
+import javafx.fxml.FXML;
+
+public class GraduatesController {
+
+
+    @FXML
+    private void initialize() {
+        // Hint: initialize() will be called when the associated FXML has been completely loaded.
+    }
+}
