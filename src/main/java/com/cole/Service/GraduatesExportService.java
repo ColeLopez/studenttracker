08867100091b4f4b08afab29c4869fde9cfffebd
Cole Lopez <lopezcole.dv@gmail.com>
2025-08-15 13:55:17 +0200

    Enhance GraduatesExportService to include additional fields in the exportGraduatesToExcel method

--- src/main/java/com/cole/Service/GraduatesExportService.java
+++ src/main/java/com/cole/Service/GraduatesExportService.java
@@ -1,5 +1,15 @@
 package com.cole.Service;
 
+import java.io.File;
+
 public class GraduatesExportService {
 
+
+    public void exportGraduatesToExcel(File file) throws Exception{
+        String[] columns ={
+            "First Name", "Second Name", "Surname", "ID Number", "Student Number",
+            "OSAP Case Number", "Programme Name", "Branch", "Email Addy"
+        };
+
+    }
 }
