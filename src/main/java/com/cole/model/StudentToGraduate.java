9350df7d3adbccf260a83413c9b4b8db09d1a177
Cole Lopez <lopezcole.dv@gmail.com>
2025-08-15 13:55:02 +0200

    Enhance StudentToGraduate model to include second name, ID number, and branch fields with corresponding getters and properties

--- src/main/java/com/cole/model/StudentToGraduate.java
+++ src/main/java/com/cole/model/StudentToGraduate.java
@@ -5,20 +5,26 @@ import javafx.beans.property.*;
 public class StudentToGraduate {
     private final StringProperty studentNumber;
     private final StringProperty firstName;
+    private final StringProperty secondName;
     private final StringProperty lastName;
+    private final StringProperty idNumber;
     private final StringProperty slpCourse;
     private final StringProperty email;
     private final StringProperty phone;
+    private final StringProperty branch;
     private final BooleanProperty transcriptRequested = new SimpleBooleanProperty();
 
-    public StudentToGraduate(String studentNumber, String firstName, String lastName,
-                             String slpCourse, String email, String phone, boolean transcriptRequested) {
+    public StudentToGraduate(String studentNumber, String firstName, String secondName, String lastName,
+                             String idNumber, String slpCourse, String email, String phone, String branch, boolean transcriptRequested) {
         this.studentNumber = new SimpleStringProperty(studentNumber);
         this.firstName = new SimpleStringProperty(firstName);
+        this.secondName = new SimpleStringProperty(secondName);
         this.lastName = new SimpleStringProperty(lastName);
+        this.idNumber = new SimpleStringProperty(idNumber);
         this.slpCourse = new SimpleStringProperty(slpCourse);
         this.email = new SimpleStringProperty(email);
         this.phone = new SimpleStringProperty(phone);
+        this.branch = new SimpleStringProperty(branch);
         this.transcriptRequested.set(transcriptRequested);
     }
 
@@ -28,8 +34,17 @@ public class StudentToGraduate {
     public String getFirstName() { return firstName.get(); }
     public StringProperty firstNameProperty() { return firstName; }
 
+    public String getSecondName() { return secondName.get(); }
+    public StringProperty secondNameProperty() { return secondName; }
+
     public String getLastName() { return lastName.get(); }
     public StringProperty lastNameProperty() { return lastName; }
+
+    public String getIdNumber() { return idNumber.get(); }
+    public StringProperty idNumberProperty() { return idNumber; }
+
+    public String getBranch() { return branch.get(); }
+    public StringProperty branchProperty() { return branch; }
 
     public String getSlpCourse() { return slpCourse.get(); }
     public StringProperty slpCourseProperty() { return slpCourse; }
@@ -46,6 +61,6 @@ public class StudentToGraduate {
 
     // For TableView "Student Name" column
     public String getFullName() {
-        return getFirstName() + " " + getLastName();
+        return getFirstName() + " " + getSecondName() + " " + getLastName();
     }
 }
