b05def7948c1139f99fe8b3c29ab0d9130ac77a7
Cole Lopez <lopezcole.dv@gmail.com>
2025-09-08 16:12:46 +0200

    Implement UserSession class for managing user session state

---
+++ src/main/java/com/cole/util/UserSession.java
@@ -0,0 +1,37 @@
+package com.cole.util;
+
+import com.cole.model.User;
+
+public class UserSession {
+    private static UserSession instance;
+    private User user;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getUserId() {
+        return user != null ? user.getId() : -1;
+    }
+
+    public String getUserRole() {
+        return user != null ? user.getRole() : null;
+    }
+
+    public void clear() {
+        user = null;
+    }
+}
